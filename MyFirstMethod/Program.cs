43be59a791aa944d3a0cf5e954816e4e92eaae82
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-16 18:58:23 +0000

    Added method CountToN but with 2 arguments and that prints between those 2 arguments

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -40,5 +40,13 @@ namespace MyFirstMethod
                 Console.WriteLine(i);
             }
         }
+
+        private static void CountToN(int n1, int n2)
+        {
+            for (int i = n1; i <= n2; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
