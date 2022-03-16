94cc06e61d31410544a61a920413ad7895d53b3e
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-16 19:10:48 +0000

    Created Fibonacci method and made main call it with the argumrnt 4 and printing it

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,7 +6,25 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Variables
+            int num = 4;
+
+            //Asks user for a number
+
+            //calls the fibonacci function with the user number
+            Console.WriteLine(Fibonacci(num));
+            
+        }
+
+        private static int Fibonacci(int n)
+        {
+            int fib;
+            if (n <= 2)
+                fib = 1;
+            else
+                fib = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+            return fib;
         }
     }
 }
