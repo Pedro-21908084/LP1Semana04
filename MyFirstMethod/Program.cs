37e1e0460a5906201f9c2455e1f9554b474a3a14
Pedro Fernandes <pm_rf@hotmail.com>
2022-03-16 18:49:13 +0000

    Added XML comments to methods CountTo10 and CountToN

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -12,11 +12,19 @@ namespace MyFirstMethod
             CountToN(12);
         }
 
+        /// <summary>
+        /// Prints from 1 to 10
+        /// ; Calls function CountToN to do it
+        /// </summary>
         private static void CountTo10()
         {
             CountToN(10);
         }
 
+        /// <summary>
+        /// Prints from 1 to n
+        /// </summary>
+        /// <param name="n">The maximun the function should print to</param>
         private static void CountToN(int n)
         {
             for (int i = 1; i <= n; i++)
